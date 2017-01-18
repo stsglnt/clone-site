75e43d7ebc5310b7f7089e70bc2c0d445afece48
Stanislav Galiant <stsglnt@gmail.com>
2017-01-18 21:35:58 +0200

    added form validation

--- project/scripts/app.js
+++ project/scripts/app.js
@@ -1,5 +1,40 @@
 angular.module('serviceApp', [])
+
+.directive('showErrors', function() {
+    return {
+      restrict: 'A',
+      require: '^form',
+      link: function (scope, el, attrs, formCtrl) {
+        // find the text box element, which has the 'name' attribute
+        var inputEl   = el[0].querySelector("[name]");
+        // convert the native text box element to an angular element
+        var inputNgEl = angular.element(inputEl);
+        // get the name on the text box
+        var inputName = inputNgEl.attr('name');
+        
+        // only apply the has-error class after the user leaves the text box
+        inputNgEl.bind('blur', function() {
+          el.toggleClass('has-error', formCtrl[inputName].$invalid);
+        })
+      }
+    }
+  })
+
 .controller('serviceCtrl', function($scope){
+  $scope.save = function() {
+    if ($scope.userForm.$valid) {
+      alert('User saved');
+      $scope.reset();
+    } else {
+      alert("There are invalid fields");
+    }
+  };
+  
+  $scope.reset = function() {
+    $scope.user = { name: '', email: '' };
+  }
+
+
     
     $scope.url = 'views/bar.html';
     
@@ -9,4 +44,9 @@ angular.module('serviceApp', [])
     $scope.changeToRolls = function(){
         $scope.url = 'views/rolls.html'
     }
-})
+});
+
+
+
+
+
